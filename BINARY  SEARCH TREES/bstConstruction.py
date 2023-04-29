b32ee00e66c51c73796e99755b842df95de690aa
Shashank Dhakate <shashankdkte@gmail.com>
2023-04-30 01:54:13 +0530

    ADDED BST CONSTRUCTION

---
+++ BINARY  SEARCH TREES/bstConstruction.py
@@ -0,0 +1,76 @@
+class BST:
+    def __init__(self,value):
+       self.value = value
+       self.left = None
+       self.right = None
+#AVERAGE      
+#TIME COMPLEXITY O(logn)
+#SPACE COMPLEXITY O(1)
+
+#WORST
+#TIME COMPLEXITY O(n)
+#SPACE COMPLEXITY O(1)
+    def insert(self,value):
+        currentNode = self
+        while True:
+            if value < currentNode.value:
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            elif value > currentNode.value:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+        return self
+
+
+    def contains(self,value):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            elif value > currentNode.value:
+                currentNode = currentNode.right    
+            elif value == currentNode.value:
+                return True
+        return False
+#AVERAGE      
+#TIME COMPLEXITY O(logn)
+#SPACE COMPLEXITY O(1)
+
+#WORST
+#TIME COMPLEXITY O(n)
+#SPACE COMPLEXITY O(1)
+    def remove(self,value,parentNode=None):
+        currentNode = self
+        while currentNode is not None:
+            if value < currentNode.value:
+                parentNode = currentNode
+                currentNode = currentNode.left
+            elif value >currentNode:
+                parentNode =currentNode
+                currentNode = currentNode.right
+            else:
+                if currentNode.left is not None and currentNode.right is not None:
+                    currentNode.value = currentNode.right.getMinValue()
+                    currentNode.right.remove(currentNode.value,currentNode)
+                elif parentNode is None:
+                    if currentNode.left is not None:
+                        currentNode.value = currentNode.left.value
+                        currentNode.right = currentNode.left.right
+                        currentNode.left = currentNode.left.left   
+                    elif currentNode.right is not None:
+                        currentNode.value = currentNode.right.value
+                        currentNode.left = currentNode.right.left
+                        currentNode.right = currentNode.right.right
+                    else:
+                        currentNode.value = None         
+                elif parentNode.left == currentNode:
+                    parentNode.left = currentNode.left if currentNode.left is not None else currentNode.right
+                elif parentNode.right == currentNode:
+                    parentNode.right = currentNode.left if currentNode.left is not None else currentNode.right
+                break
