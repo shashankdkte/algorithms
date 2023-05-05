bd73ec12b81b47f10a7d7f0586c7cb7546ae03e4
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-05 19:29:20 +0530

    ADDED SINGLE CYCLE CHECK

---
+++ GRAPHS/singleCycleCheck.py
@@ -0,0 +1,16 @@
+#TIME COMPLEXITY O(N)
+#SPACE COMPLEXITY O(1)
+def hassingleCycle(array):
+    numElementVisited = 0;
+    currentIdx = 0
+    while numElementVisited < len(array):
+        if numElementVisited > 0  and currentIdx == 0:
+            return False
+        numElementVisited +=1
+        currentIdx = getNextIdx(currentIdx,array)
+    return currentIdx == 0
+
+def getNextIdx(currentIdx,array):
+    jump = array[currentIdx];
+    nextIdx = (currentIdx + jump)%len(array)
+    return nextIdx if nextIdx >=0 else nextIdx + len(array)
