6063cb17b12c1f290a7cf4db071c24531a2e630d
Shashank Dhakate <shashankdkte@gmail.com>
2023-05-10 10:44:47 +0530

    ADDED PRODUCT SUM

---
+++ RECURSION/productSum.py
@@ -0,0 +1,10 @@
+# TIME COMPLEXITY O(n)
+# SPACE COMPLEXITY O(d)
+def productSum(array,multiplier=1):
+    sum = 0
+    for element in array:
+        if type(element) is list:
+            sum += productSum(element,multiplier+1)
+        else:
+            sum += element
+    return sum * multiplier              
